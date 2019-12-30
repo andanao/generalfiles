66caf8c57752ec828b76de53ac883262c5732271
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2019-12-30 10:48:46 -0500

    playing with matplotlib

---
+++ Paper Designs/Journal/2020_Author.py
@@ -0,0 +1,14 @@
+from baisc_units import cm, inch
+import matplotlib.pyplot as plt
+from matplotlib.collections import EventCollection
+import numpy as np
+
+
+# ALL UNITS IN MM
+width = 130 
+height = 210
+
+
+rpage = plt.figure()
+plt.plot(range(10),range(10),'o')
+plt.show()
