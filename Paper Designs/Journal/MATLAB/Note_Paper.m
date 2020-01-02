158deaf28c1936b06b32c61edc87761d27fb57f3
andanao <47489123+andanao@users.noreply.github.com>
2020-01-01 23:58:25 -0500

    minor changes to notepaper after usage

--- Paper Designs/Journal/MATLAB/Note_Paper.m
+++ Paper Designs/Journal/MATLAB/Note_Paper.m
@@ -15,7 +15,7 @@ spacing = .25;  %change to .17 ish
 
 marg = 1.5*spacing; 
 right = width-marg;
-top = height-marg-1.5*spacing;
+top = height-(marg+1.5*spacing);
 bottom = marg;
 left = marg;
 
@@ -27,14 +27,20 @@ line_mat(:,3:4)= [lineheights; lineheights]';
 line_mat = line_mat+1;
 
 %tab in lines 1 and 4
-line_mat(end,1) = line_mat(end,1)+(right-left)/5;
+line_mat(end,1) = line_mat(end,1)+(right-left)/8;
 line_mat(end-3,1) = line_mat(end,1);
 
+top = max(max(lineheights));
 
 plot(1+[0,width,width,0,0],1+[0,0,height,height,0],'-.k.','LineWidth',.25) %page outline
 plot(line_mat(:,1:2)',line_mat(:,3:4)','-k.','LineWidth',2) %actual writing lines
 
 plot(1+[left,right/2],1+[top+1*spacing,top+1*spacing],'-k.','LineWidth',2) %date line
+
+%extended lines for multi page entries
+plot(1+[left,right],1+[top+1*spacing,top+1*spacing],':k.','LineWidth',1)
+plot(1+[left,right],1+[top,top],':k.','LineWidth',1)
+plot(1+[left,right],1+[top-3*spacing,top-3*spacing],':k.','LineWidth',1)
 
 plot(1+[left,left;right,right]',[1+bottom,line_mat(end-1,end);1+bottom,line_mat(end,end)]',':k.','LineWidth',1) %block end lines
 
@@ -48,8 +54,8 @@ hAxes.Position = [0 0 1 1];
 axis off;
 name = strcat('The Author-',datestr(now,'yyyy-mm-dd'),'.pdf');
 
-saveas(fig,name) 
-open(name)
+% saveas(fig,name) 
+% open(name)
 
 
 % close all
